8bbd0b896dbae6c2726ca27316951b43a1109b71
Test developer <test@testmail.com>
2016-12-15 17:37:51 +0530

    RED

--- harry.potter/src/test/java/com/clean/books/harry/potter/BookBasketShould.java
+++ harry.potter/src/test/java/com/clean/books/harry/potter/BookBasketShould.java
@@ -28,7 +28,7 @@ public class BookBasketShould {
 		basket.add(Book.HARRY_POTTER_BOOK_3);
 		assertEquals(21.6d, basket.getBasketPrice(), 0);
 	}
-	
+
 	@Test
 	public void return_price_for_different_4_books() {
 		BookBasket basket = new BookBasket();
@@ -38,6 +38,7 @@ public class BookBasketShould {
 		basket.add(Book.HARRY_POTTER_BOOK_4);
 		assertEquals(25.6d, basket.getBasketPrice(), 0);
 	}
+
 	@Test
 	public void return_price_for_different_5_books() {
 		BookBasket basket = new BookBasket();
@@ -47,5 +48,15 @@ public class BookBasketShould {
 		basket.add(Book.HARRY_POTTER_BOOK_4);
 		basket.add(Book.HARRY_POTTER_BOOK_5);
 		assertEquals(30d, basket.getBasketPrice(), 0);
+	}
+
+	@Test
+	public void return_price_for_three_differen_and_two_same_books() {
+		BookBasket basket = new BookBasket();
+		basket.add(Book.HARRY_POTTER_BOOK_1);
+		basket.add(Book.HARRY_POTTER_BOOK_2);
+		basket.add(Book.HARRY_POTTER_BOOK_3);
+		basket.add(Book.HARRY_POTTER_BOOK_1);
+		assertEquals(29.6d, basket.getBasketPrice(), 0);
 	}
 }
